48925a32e2985b6d33a5454c4368522cdf37a6b6
DESKTOP-G3GHCGQ\user <bpriya2001@gmail.com>
2024-07-24 05:42:30 +1000

    data transformation code completed, needs to be tested

--- src/components/dats_transformation.py
+++ src/components/dats_transformation.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -65,19 +66,46 @@ class DataTransformation:
         This function starts the data transformation
         '''
         try:
+
+            # we need to pass the train and test data through preprocessing object to transform the data
+            logging.info("Read the train and test data is completed")
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
 
+            logging.info("Obtain the preprocessing object")
             preprocessing_obj = self.get_data_transformer_object()
 
             target_col = "reading score"
             numerical_col = ["math score", "writing score"]
 
+            logging.info("Assign the input and target data for train and test")
             input_feature_train = train_df.drop(columns=[target_col], axis = 1)
             target_feature_train = train_df[target_col]
 
             input_feature_test = test_df.drop(columns=[target_col], axis = 1)
             target_feature_test = test_df[target_col]
+
+            input_feature_train_array = preprocessing_obj.fit_transform(input_feature_train)
+            input_feature_test_array = preprocessing_obj.transform(input_feature_test)
+
+            # np.c_[np.array([1,2,3]), np.array([4,5,6])] = array([1,4],[2,5],[3,6])
+            train_array = np.c_[input_feature_train_array, np.array(target_feature_train)]
+            test_array = np.c_[input_feature_test_array, np.array(target_feature_test)]
+
+            # save the preprocessing object in a pkl file for reuse
+            # this function is in the util file
+            logging.info("Saving the preprocessor object in pkl file")
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_file_path,
+                obj = preprocessing_obj
+            )
+
+            return (
+                train_array,
+                test_array,
+                self.data_transformation_config.preprocessor_file_path,
+            )
+
 
         except Exception as e:
             raise CustomException(e, sys)
