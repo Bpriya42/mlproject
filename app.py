a1e59e876750c5ebdeedc525300b51eec52d86fc
DESKTOP-G3GHCGQ\user <bpriya2001@gmail.com>
2024-07-25 19:12:22 +1000

    application completed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
@@ -15,6 +16,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/predictdata', methods = ['GET', 'POST'])
+
 def predict_datapoint():
     '''Get and predict data'''
 
@@ -23,5 +25,24 @@ def predict_datapoint():
     else:
         # capture data
         # standard scaling or feature scaling
+
+        data = CustomData(
+            gender = request.form.get('gender'),
+            race_ethnicity = request.form.get('race_ethnicity'),
+            parental_level_of_education = request.form.get('parental_level_of_education'),
+            lunch = request.form.get('lunch'),
+            test_preparation_course = request.form.get('test_preparation_course'),
+            math_score = request.form.get('math_score'),
+            writing_score = request.form.get('writing_score')
+        )
+
+        prediction_df = data.get_data_as_df()
+
+        prediction_pipeline = PredictPipeline()
+
+        results = prediction_pipeline.predict(prediction_df)
+
+        return render_template("home.html", results = results[0])
         
-        pass
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
